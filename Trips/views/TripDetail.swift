2b768b41f4a085594a86bb7b54edd43854878bc0
Lucas Kellar <lucas@lkellar.org>
2019-12-03 10:09:59 -0600

    Fixed #2
    Also fixed the delete the wrong item bug as well. With some great hacks!

--- Trips/views/TripDetail.swift
+++ Trips/views/TripDetail.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct TripDetail: View {
     @Environment(\.managedObjectContext) var context
@@ -14,22 +15,32 @@ struct TripDetail: View {
     @State var modalDisplayed = false
     @State var packModalDisplayed = false
     @State var editTripDisplayed = false
+    @State var refreshing = false
     
     var trip: Trip
+
+    var packRequest : FetchRequest<Pack>
     
-    @FetchRequest(fetchRequest: Pack.allPacksFetchRequest()) var packs: FetchedResults<Pack>
-    
-    @FetchRequest(fetchRequest: Item.allItemsFetchRequest()) var items: FetchedResults<Item>
+    var packs: FetchedResults<Pack>{packRequest.wrappedValue}
+    init(trip: Trip) {
+        self.trip = trip
+        self.packRequest = FetchRequest(entity: Pack.entity(),sortDescriptors: [NSSortDescriptor(key: "name", ascending: true)], predicate:
+            NSPredicate(format: "%K == %@", #keyPath(Pack.trip), trip))
+    }
     
     var body: some View {
         List {
-            ForEach(self.packs.filter {$0.trip == self.trip} ) {pack in
-                Section(header: Text(pack.name)) {
-                    ForEach(self.items.filter {$0.pack == pack}) { item in
+            ForEach(self.packs ) {pack in
+                // Same hack used in TripHomeRow.swift, but A. it seems to work, and B. I can't find another way around it. Basically, it manually refereshes view
+                Section(header: Text(pack.name + (self.refreshing ? "" : ""))) {
+                    ForEach(self.fetchItems(pack)) { item in
                         Text(item.name)
                     }.onDelete(perform: self.getDeleteFunction(pack: pack))
                 }
             }
+            
+            
+            //Text(refreshing ? "" : "")
             }.navigationBarTitle(trip.name)
             .navigationBarItems(trailing:
                 HStack {
@@ -64,11 +75,32 @@ struct TripDetail: View {
             })
     }
     
+    func fetchItems(_ pack: Pack) -> [Item] {
+        let request: NSFetchRequest<Item> = Item.fetchRequest() as! NSFetchRequest<Item>
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        request.predicate = NSPredicate(format: "%K == %@", #keyPath(Item.pack), pack)
+        
+        do {
+            return try self.context.fetch(request)
+        } catch {
+            print(error)
+            return []
+        }
+        
+    }
+    
     func getDeleteFunction(pack: Pack) -> (IndexSet) -> Void {
         func delete(at offsets: IndexSet) {
-            pack.removeFromItems(at: NSIndexSet(indexSet: offsets))
+            let items = self.fetchItems(pack)
             
+            for offset in offsets {
+                pack.removeFromItems(items[offset])
+            }
+                        
             saveContext(self.context)
+            self.refreshing.toggle()
             
         }
         
